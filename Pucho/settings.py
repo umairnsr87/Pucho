8d696051363857263e181fd6e34cc37261f8cc1d
umair ansari <umairnsr87@gmail.com>
2020-04-03 01:02:59 +0530

    Html updated

--- Pucho/settings.py
+++ Pucho/settings.py
@@ -119,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 print("the base path is"+BASE_DIR)
